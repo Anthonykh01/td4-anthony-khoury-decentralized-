1c4319727190f43aa574fdc5631d8cc9c38e9be8
anthonykh01 <anthonyykh01@gmail.com>
2024-03-06 01:29:10 +0200

    26 done

--- src/registry/registry.ts
+++ src/registry/registry.ts
@@ -20,7 +20,31 @@ export async function launchRegistry() {
 
   // TODO implement the status route
   // _registry.get("/status", (req, res) => {});
+  _registry.get("/status", (req: Request, res: Response) => {
+    res.send("live");
+  });
 
+  
+  let nodeRegistry: Node[] = [];
+
+  _registry.post('/registerNode', (req: Request, res: Response) => {
+    const { nodeId, pubKey } = req.body;
+
+    const newNode: Node = {
+      nodeId: nodeId,
+      pubKey: pubKey,
+    };
+
+    nodeRegistry.push(newNode);
+
+    return res.status(200).send({ message: "Node registered successfully." });
+  });
+
+  _registry.get("/getNodeRegistry", (req, res) => {
+    res.json({ nodes: nodeRegistry });
+  });
+
+  
   const server = _registry.listen(REGISTRY_PORT, () => {
     console.log(`registry is listening on port ${REGISTRY_PORT}`);
   });
